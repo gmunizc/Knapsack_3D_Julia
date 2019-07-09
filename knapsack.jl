3b33223a7dcea7a0975a696cf387fcf777c88adb
Gabriel Muniz <futlouco7@gmail.com>
2019-07-08 21:14:32 -0300

    Update knapsack.jl

--- knapsack.jl
+++ knapsack.jl
@@ -139,8 +139,60 @@ function get_hi_dim_and_wi_dim(relations)
     (hi = dim_dict[ordered_relations[3][2][1]], wi = dim_dict[ordered_relations[1][2][1]])
 end
 
+function add3DTuple(tuple1, tuple2)
+    partial_results = [0]
+
+    for i in 1:length(tuple1)
+        push!(partial_results, tuple1[i] + tuple2[i])
+    end
+
+    return new_tuple = (partial_results[2], partial_results[3], partial_results[4])
+end
+
+function remove!(a, item)
+    deleteat!(a, findall(x->x==item, a))
+end
+
+function item_actual_orientation(item_dim, orientation)
+    dim_dict = Dict("ai" => 1, "bi" => 2, "ci" => 3)
+    list_dim = [1, 2, 3]
+    z = orientation[1]
+    x = orientation[2]
+    item_x = item_dim[dim_dict[x]]
+    remove!(list_dim, dim_dict[x])
+    item_z = item_dim[dim_dict[z]]
+    remove!(list_dim, dim_dict[z])
+    item_y = item_dim[list_dim[1]]
+
+    return (item_x, item_y, item_z)
+end
+
 function calculate_new_corners(J, knapsack)
-    return list_of_possible_corners
+    new_corners = [(xi = 0, yi = 0, zi = 0)]
+
+    if length(J) == 1
+        return new_corners
+    end
+    
+    for i in 1:length(J)
+        item = J[i][1]
+        coordinates = J[i][2]
+        orientation = J[i][3]
+
+        oriented_item = item_actual_orientation(item.dim, orientation)
+        
+        new_corner_1 = add3DTuple(coordinates, (oriented_item[1], 0, 0))
+        push!(new_corners, (xi = new_corner_1[1], yi = new_corner_1[2], zi = new_corner_1[3]))
+        new_corner_2 = add3DTuple(coordinates, (0, oriented_item[2], 0))
+        push!(new_corners, (xi = new_corner_2[1], yi = new_corner_2[2], zi = new_corner_2[3]))
+        new_corner_3 = add3DTuple(coordinates, (0, 0, oriented_item[3]))
+        push!(new_corners, (xi = new_corner_3[1], yi = new_corner_3[2], zi = new_corner_3[3]))
+
+    end
+
+    popfirst!(new_corners)
+
+    return new_corners
 end
 
 function add_to_knapsack(item, J, knapsack, list_of_possible_corners)
@@ -155,15 +207,16 @@ function add_to_knapsack(item, J, knapsack, list_of_possible_corners)
                 ordered_space_dims = order_relations_in_xyz(relations)
                 hi_wi_dims = get_hi_dim_and_wi_dim(relations)
                 push!(J,(item, (xi = corner.xi, yi = corner.yi, zi = corner.zi), hi_wi_dims))
+                break
             end
         end
     end
-    return J
+    # return J
 end
 
-knapsack_dim = 100
+knapsack_dim = 10
 knapsack = (H = knapsack_dim, W = knapsack_dim, L = knapsack_dim)
-list_of_possible_corners = [(xi = 0, yi = 0, zi = 0)]
+
 items = [(vi = 10, dim = (ai = 3, bi = 7, ci = 13)), (vi = 105, dim = (ai = 5, bi = 3, ci = 17)),
          (vi = 20, dim = (ai = 6, bi = 3, ci = 4)), (vi = 95, dim = (ai = 3, bi = 1, ci = 1)),
          (vi = 30, dim = (ai = 9, bi = 5, ci = 9)), (vi = 85, dim = (ai = 6, bi = 7, ci =23)), 
@@ -177,10 +230,17 @@ items = [(vi = 10, dim = (ai = 3, bi = 7, ci = 13)), (vi = 105, dim = (ai = 5, b
 V = 0
 item_zero = (vi = 0, dim = (ai = 0, bi = 0, ci = 0))
 J = [(item_zero, (xi = 0, yi = 0, zi = 0), (hi = "ai", wi = "bi"))]
-J = add_to_knapsack(items[1], J, knapsack, list_of_possible_corners)
+list_of_possible_corners = calculate_new_corners(J, knapsack)
+add_to_knapsack(items[1], J, knapsack, list_of_possible_corners)
 popfirst!(J)
+for i in 2:length(items)
+    list_of_possible_corners = calculate_new_corners(J, knapsack)
+    add_to_knapsack(items[i], J, knapsack, list_of_possible_corners)
+end
 println("J: ")
 println(J)
+print("length J: " )
+println(length(J))
 
 
 
